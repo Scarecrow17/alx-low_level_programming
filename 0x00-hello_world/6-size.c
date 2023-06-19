e56727392a85a8d4d33e00b388c04885c82c6849
Prudence Silinda <prudencesilinda2016@gmail.com>
2023-06-19 16:05:42 -0700

    prints the size of various types on the computer it is compiled and run on

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - prints the size of various types on the computer
- * Return: 0 if exited properly, non-zero otherwise
+ * main - prints the size of various types on the computer it is compiled and run on
+ * Return: 0 
  */
 int main(void)
 {
