1a6491e824dc2449fb1d34212fc04c9b55a70c57
Prudence Silinda <prudencesilinda2016@gmail.com>
2023-06-19 16:10:06 -0700

    prints the size of various types on the computer it is compiled and run on

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - size of various types 
- * on the computer it is compiled
- * Return: 0 
+ * main - size of various types
+ *
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
