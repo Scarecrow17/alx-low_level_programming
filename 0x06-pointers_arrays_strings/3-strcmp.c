2b28d84fb0c43d68c913b4eb1ac543ad7343793f
Prudence Silinda <prudencesilinda2016@gmail.com>
2023-06-29 18:59:03 -0700

    edit

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,19 +4,22 @@
  * _strcmp - compare two strings
  * @s1: string one
  * @s2: string two
- * Return: int
  */
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 == *s2)
+	while (((*s1 != '\0') && (*s2 != '\0')) && (*s1 == *s2))
 	{
-		if (*s1 == '\0')
-		{
-			return (0);
-		}
 		s1++;
 		s2++;
 	}
-	
-	return (*s1 - *s2);
+
+	if (*s1 == *s2)
+	{
+		return (0);
+	}
+
+	else
+	{
+		return (*s1 - *s2);
+	}
 }
