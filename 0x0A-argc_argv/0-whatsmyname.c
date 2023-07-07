a98a4c028dde2f318404083ce89594da2b248629
Prudence Silinda <prudencesilinda2016@gmail.com>
2023-07-07 13:24:43 -0700

    print name then new line

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints program name then new line
+ * @argc: number of arguments
+ * @argv: arguments array
+ *
+ * Return: 0 (success)
+ */
+
+int main(int argc, char **argv)
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,6 @@
+0: print name, followed by a new line
+1: print number of arguments passed into it
+2: print arguments it receives
+3: multiply two numbers
+4: Add positive numbers
+5: print minimum number of coins to make change for an amount of money
