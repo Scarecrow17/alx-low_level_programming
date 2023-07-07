e1cb57c25a697518298a8978e7b43d30c01ddbf9
Prudence Silinda <prudencesilinda2016@gmail.com>
2023-07-07 13:43:50 -0700

    add positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - add +ve numbers
+ * @argc: number of arguments
+ * @argv: arguments array
+ * Return: 0 or 1(success)
+ */
+
+int main(int argc, char **argv)
+{
+	int p, n, value = 0;
+	char *flag;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (p = 1; argv[p]; p++)
+	{
+		x = strtol(argv[p], &flag, 10);
+		if (*flag)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+		{
+			value += x;
+		}
+	}
+	printf("%d\n", value);
+
+	return (0);
+}
