3423be832652954186900dfbdeb68d3a99515d84
Prudence Silinda <prudencesilinda2016@gmail.com>
2023-07-13 13:07:50 -0700

    allocate memory using malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocate memory using malloc
+ * @b: byte number allocated
+ * Return: allocated memory pointer
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *x;
+
+	x = malloc(b);
+	if (x == NULL)
+		exit(98);
+	return (x);
+
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,6 @@
+0: allocate memory using malloc
+1: concatenate two strings
+2: allocate memory for an array, using malloc
+3: create array of integers
+4: reallocates memory block using malloc and free
+5: multiply two positive numbers
