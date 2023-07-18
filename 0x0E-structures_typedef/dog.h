a961bcddf095040d56bdb4a3b436572f4b92b865
Prudence Silinda <prudencesilinda2016@gmail.com>
2023-07-18 07:32:31 -0700

    edit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,10 +16,11 @@ struct dog
 };
 
 /**
- * dog_t - typedef
+ * dog_t - struct dog typedef
  */
 
 typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
