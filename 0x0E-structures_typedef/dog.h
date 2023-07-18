3df0a96b71141567f660c1041761bfe45693c17f
Prudence Silinda <prudencesilinda2016@gmail.com>
2023-07-18 07:33:38 -0700

    edit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,7 +2,7 @@
 #define DOG_H
 
 /**
- * struct dog - The dog's basic info & class
+ * struct dog - dog's basic info & class
  * @name: dog name
  * @age: dog age
  * @owner: dog owner
@@ -27,4 +27,5 @@ dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
+
 #endif
