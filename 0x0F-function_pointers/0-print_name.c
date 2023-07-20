3f4a3325b32db74ea0535bc7648709a2048b2997
Prudence Silinda <prudencesilinda2016@gmail.com>
2023-07-20 10:44:37 -0700

    print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - print name
+ * @name: given name
+ * @f: printing function pointer
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (!name || !f)
+
+		return;
+	f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,5 @@
+0: print a name
+1: execute function given as a parameter on each element of an array
+2: search for an integer
+3: perform simple operations
+4: print opcodes of its own main function
