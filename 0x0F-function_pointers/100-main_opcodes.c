f5d77df66527ed3961870f9a442b5e4b13a13e25
Prudence Silinda <prudencesilinda2016@gmail.com>
2023-07-20 11:53:01 -0700

    prints the opcodes of its own main function

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print own opcodes of program
+ * @argc: argument count
+ * @argv: argument array
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int bytes, p;
+	char *array;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	bytes = atoi(argv[1]);
+
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	array = (char *)main;
+
+	for (p = 0; p < bytes; p++)
+	{
+		if (p == bytes - 1)
+		{
+			printf("%02hhx\n", array[p]);
+			break;
+		}
+		printf("%02hhx ", arr[p]);
+	}
+	return (0);
+}
