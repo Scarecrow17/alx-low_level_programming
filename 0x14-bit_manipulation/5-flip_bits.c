84e2a94567351062706b04b0cf30ef4087045ee7
Prudence Silinda <prudencesilinda2016@gmail.com>
2023-08-08 11:48:41 -0700

    Task 5

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -10,17 +10,19 @@
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned long int difs;
-	int count;
+	unsigned long int diff, answer;
+	unsigned int q, p;
 
-	difs = n ^ m;
-	count = 0;
+	q = 0;
+	answer = 1;
+	diff = n ^ m;
 
-	while (difs)
+	for (p = 0; p < (sizeof(unsigned long int) * 8); p++)
 	{
-		count++;
-		difs &= (difs - 1);
+		if (answer == (diff & answer))
+			q++;
+		answer <<= 1;
 	}
 
-	return (counter);
+	return (q);
 }
