d994030dfeaf7e2a3b4b7e66c5d7ae055f3cca2b
Scarecrow17 <prudencesilinda2016@gmail.com>
2023-10-20 18:12:43 -0700

    edit

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -3,9 +3,8 @@
 /**
  * get_dnodeint_at_index - returns the
  * nth node of a dlistint_t linked list
- *
- * @head: head 
- * @index: index 
+ * @head: head
+ * @index: index
  * Return: nth node
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
