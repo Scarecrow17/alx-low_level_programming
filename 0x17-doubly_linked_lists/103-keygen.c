05cad420270ef682a5cbfd0e2cbabd39408c240e
Scarecrow17 <prudencesilinda2016@gmail.com>
2023-10-20 18:30:39 -0700

    edit

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -3,121 +3,124 @@
 #include <time.h>
 
 /**
- * f4 - find biggest number
+ * f4 - finds number
  * @usrn: username
- * @len: length 
- * Return: biggest
+ * @len: length
+ * Return: biggest number
  */
 int f4(char *usrn, int len)
 {
-	int char;
+	int ch;
 	int vch;
-	unsigned int rand;
+	unsigned int rand_num;
 
-	char = *usrn;
+	ch = *usrn;
 	vch = 0;
 
 	while (vch < len)
 	{
-		if (char < usrn[vch])
-			char = usrn[vch];
+		if (ch < usrn[vch])
+			ch = usrn[vch];
 		vch += 1;
 	}
 
 	srand(ch ^ 14);
-	rand = rand();
+	rand_num = rand();
 
-	return (rand & 63);
+	return (rand_num & 63);
 }
 
 /**
  * f5 - multiplies each char of username
+ *
  * @usrn: username
- * @len: length
- * Return: multiplied
+ * @len: length of username
+ * Return: multiplied char
  */
 int f5(char *usrn, int len)
 {
-	int char;
+	int ch;
 	int vch;
 
-	char = vch = 0;
+	ch = vch = 0;
 
 	while (vch < len)
 	{
-		char = char + usrn[vch] * usrn[vch];
+		ch = ch + usrn[vch] * usrn[vch];
 		vch += 1;
 	}
 
-	return (((unsigned int)char ^ 239) & 63);
+	return (((unsigned int)ch ^ 239) & 63);
 }
 
 /**
- * f6 - random char
+ * f6 - generates a random char
+ *
  * @usrn: username
- * Return: random
+ * Return: a random char
  */
 int f6(char *usrn)
 {
-	int char;
+	int ch;
 	int vch;
 
-	char = vch = 0;
+	ch = vch = 0;
 
 	while (vch < *usrn)
 	{
-		char = rand();
+		ch = rand();
 		vch += 1;
 	}
 
-	return (((unsigned int)char ^ 229) & 63);
+	return (((unsigned int)ch ^ 229) & 63);
 }
 
 /**
  * main - Entry point
- * @argc:  count
- * @argv:  vector
- * Return:  0
+ *
+ * @argc: arguments count
+ * @argv: arguments vector
+ * Return: Always 0
  */
 int main(int argc, char **argv)
 {
 	char keygen[7];
-	int length, char, vch;
+	int len, ch, vch;
 	long alph[] = {
 		0x3877445248432d41, 0x42394530534e6c37, 0x4d6e706762695432,
 		0x74767a5835737956, 0x2b554c59634a474f, 0x71786636576a6d34,
 		0x723161513346655a, 0x6b756f494b646850 };
 	(void) argc;
 
-	for (length = 0; argv[1][length]; length++)
+	for (len = 0; argv[1][len]; len++)
 		;
 	/* ----------- f1 ----------- */
-	keygen[0] = ((char *)alph)[(length ^ 59) & 63];
+	keygen[0] = ((char *)alph)[(len ^ 59) & 63];
 	/* ----------- f2 ----------- */
-	char = vch = 0;
-	while (vch < length)
+	ch = vch = 0;
+	while (vch < len)
 	{
-		char = char + argv[1][vch];
+		ch = ch + argv[1][vch];
 		vch = vch + 1;
 	}
-	keygen[1] = ((char *)alph)[(char ^ 79) & 63];
+	keygen[1] = ((char *)alph)[(ch ^ 79) & 63];
 	/* ----------- f3 ----------- */
-	char = 1;
+	ch = 1;
 	vch = 0;
 	while (vch < len)
 	{
-		char = argv[1][vch] * char;
+		ch = argv[1][vch] * ch;
 		vch = vch + 1;
 	}
-	keygen[2] = ((char *)alph)[(char ^ 85) & 63];
+	keygen[2] = ((char *)alph)[(ch ^ 85) & 63];
 	/* ----------- f4 ----------- */
-	keygen[3] = ((char *)alph)[f4(argv[1], length)];
+	keygen[3] = ((char *)alph)[f4(argv[1], len)];
 	/* ----------- f5 ----------- */
-	keygen[4] = ((char *)alph)[f5(argv[1], length)];
+	keygen[4] = ((char *)alph)[f5(argv[1], len)];
 	/* ----------- f6 ----------- */
 	keygen[5] = ((char *)alph)[f6(argv[1])];
 	keygen[6] = '\0';
-	for (char = 0; keygen[char]; char++)
-		printf("%c", keygen[char]);
+	for (ch = 0; keygen[ch]; ch++)
+		printf("%c", keygen[ch]);
 	return (0);
 }
