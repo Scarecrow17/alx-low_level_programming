423847b9c99b0c98915c30fbf0dda7268adae94b
Scarecrow17 <prudencesilinda2016@gmail.com>
2023-10-20 18:09:45 -0700

    edit

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 
 /**
- * add_dnodeint_end - adds  end
- * @head: head 
- * @n: value 
- * Return:  address 
+ * add_dnodeint_end - adds end
+ * @head: head
+ * @n: value
+ * Return: address
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
